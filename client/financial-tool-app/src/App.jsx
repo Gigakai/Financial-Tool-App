dfb4b3f78b6c8f7342879ac1bd9c3b1ea3c12099
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2025-10-25 08:31:21 -0600

    Last Changes made

--- client/financial-tool-app/src/App.jsx
+++ client/financial-tool-app/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import MainLayout from "./components/MainLayout.jsx";
